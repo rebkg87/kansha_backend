cacf5c546a5eb4a6b667cb1979778091879b1901
Rebeca Garcia <rebe.ga87@gmail.com>
2024-09-05 12:20:58 +0200

    first commit

---
+++ src/main/java/com/example/kansha/KanshaApplication.java
@@ -0,0 +1,16 @@
+package com.example.kansha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanshaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(KanshaApplication.class, args);
+		System.out.println("Hello, welcome back");
+
+	}
+
+}
