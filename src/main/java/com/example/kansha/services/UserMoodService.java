798fd2ea30d15a75586edda9ee359f0648a9ae7f
Rebeca Garcia <rebe.ga87@gmail.com>
2024-09-16 23:55:08 +0200

    add mood & userMood services

---
+++ src/main/java/com/example/kansha/services/UserMoodService.java
@@ -0,0 +1,27 @@
+package com.example.kansha.services;
+
+import com.example.kansha.models.Mood;
+import com.example.kansha.models.User;
+import com.example.kansha.models.UserMood;
+import com.example.kansha.repositories.UserMoodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class UserMoodService {
+
+    @Autowired
+    private UserMoodRepository userMoodRepository;
+
+    public List<UserMood> findByUserAndMoodDate (User user, Date moodDate) {
+        return userMoodRepository.findByUserAndMoodDate(user, moodDate);
+    }
+
+    public List<UserMood>findByUserAndMood(User user, Mood mood) {
+        return  userMoodRepository.findByUserAndMood(user, mood);
+    }
+
+}
