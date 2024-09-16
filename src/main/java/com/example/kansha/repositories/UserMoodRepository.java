a463c7d8baec2104d2d6a51d6e8809c16dead3a5
Rebeca Garcia <rebe.ga87@gmail.com>
2024-09-16 23:03:27 +0200

    add mood & userMood repositories

---
+++ src/main/java/com/example/kansha/repositories/UserMoodRepository.java
@@ -0,0 +1,16 @@
+package com.example.kansha.repositories;
+
+import com.example.kansha.models.Mood;
+import com.example.kansha.models.User;
+import com.example.kansha.models.UserMood;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+
+@Repository
+public interface UserMoodRepository extends JpaRepository {
+    List<UserMood>findByUserAndMoodDate(User user, Date moodDate);
+    List<UserMood>findByUserAndMood(User user, Mood mood);
+}
